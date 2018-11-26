f6fc88d7011dd5a7a758786dbf4e7da838dfd075
FireController1847 <usfirepilot123@gmail.com>
2018-11-26 00:48:53 -0700

    Add Tests

---
+++ test/index.md
@@ -0,0 +1,10 @@
+### Step 1
+- Create a file named "username" with no extension.
+- Put your username in the file.
+- That's it. Don't do anything else.
+- No markdown. At all. No strings. Nothing. Just the username.
+
+### Step 2
+- Create a file named "password" with no extension.
+- Put your password in the file.
+- Same thing. No markdown, no strings, no quotations. It is literally just your password.
---
+++ test/test.js
@@ -0,0 +1,32 @@
+process.chdir("./test");
+const { Client } = require("../src");
+const assert = require("assert");
+const client = new Client();
+const fs = require("fs");
+
+describe("Attempt a login and ensure it's successful", async function() {
+
+  it("Run a login attempt", async function() {
+    await client.login(fs.readFileSync("./username").toString(), fs.readFileSync("./password").toString());
+  }).timeout(10000);
+
+  it("Check for tokens", async function() {
+    if (!client.rest.getCSRFToken()) assert.fail("CSRF token is unavailabe!");
+    if (!client.user || !client.user.token) assert.fail("User token is unavailable!");
+  });
+
+});
+
+describe("Attempt to fetch stations", async function() {
+
+  it("Run a station fetch", async function() {
+    await client.user.getStations();
+  });
+
+  it("Check for required station", async function() {
+    const station = client.user.stations.first();
+    if (!station) assert.fail("The user does not have any stations!");
+    if (station.name != "Thumbprint Radio") assert.fail("First station is not Thumbprint Radio!");
+  });
+
+});
