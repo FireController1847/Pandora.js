705f5004229f1c0f3126c37e4d1114a42b0389ff
FireController1847 <usfirepilot123@gmail.com>
2018-11-27 16:42:27 -0700

    More Linting & Attempt Coverage

--- test/test.js
+++ test/test.js
@@ -4,29 +4,25 @@ const assert = require("assert");
 const client = new Client();
 const fs = require("fs");
 
-describe("Attempt a login and ensure it's successful", async function() {
-
-  it("Run a login attempt", async function() {
+describe("Attempt a login and ensure it's successful", () => {
+  it("Run a login attempt", async () => {
     await client.login(process.env.PANDORA_USERNAME || fs.readFileSync("./username").toString(), process.env.PANDORA_PASSWORD || fs.readFileSync("./password").toString());
-  }).timeout(10000);
+  });
 
-  it("Check for tokens", async function() {
+  it("Check for tokens", () => {
     if (!client.rest.getCSRFToken()) assert.fail("CSRF token is unavailabe!");
     if (!client.user || !client.user.token) assert.fail("User token is unavailable!");
   });
-
 });
 
-describe("Attempt to fetch stations", async function() {
-
-  it("Run a station fetch", async function() {
+describe("Attempt to fetch stations", () => {
+  it("Run a station fetch", async () => {
     await client.user.getStations();
   });
 
-  it("Check for required station", async function() {
+  it("Check for required station", () => {
     const station = client.user.stations.first();
     if (!station) assert.fail("The user does not have any stations!");
     if (station.name != "Thumbprint Radio") assert.fail("First station is not Thumbprint Radio!");
   });
-
 });
